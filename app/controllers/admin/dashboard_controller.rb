d83697a1d64426bf31fd7079536b50dc39d2dc17
cchand-vanc <cmchandler@live.co.uk>
2023-03-08 22:54:13 -0800

    Add @total_categoies

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,5 +3,7 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['HTTP_BASIC_AUTH_USERNAME'], password: ENV['HTTP_BASIC_AUTH_PASSWORD'], except: :index
 
   def show
+    @total_quantity = Product.sum(:quantity)
+    @total_categories = Category.count(:name)
   end
 end
