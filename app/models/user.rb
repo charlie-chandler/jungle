804ecad8d69af85ca6131d7680cecff7b54993b2
cchand-vanc <cmchandler@live.co.uk>
2023-03-13 21:55:19 -0700

    Define autheticate_with_credentials method

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,15 @@ class User < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true, uniqueness: {case_sensitive: false}
-  
+  validates :password, presence: true, length: {minimum: 3}
+
+  def self.authenticate_with_credentials(email, password)
+    user = User.find_by(email: email.strip.downcase)
+    if user && user.authenticate(password)
+      return user
+    else 
+      nil
+    end
+  end
+
 end
