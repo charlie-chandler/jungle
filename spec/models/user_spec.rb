7dc2ac03d20b5aa3ed5fd4ded491d68154356837
cchand-vanc <cmchandler@live.co.uk>
2023-03-13 19:14:13 -0700

    Adds password validation tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'must have password' do
+    user = User.new(name: 'Charlie', email: 'charlie@test.com', password: '123', password_confirmation: '123')
+    expect(user.password).to_not be nil
+end
+
+it 'must have a password and password confirmation that match' do
+  user = User.new(name: 'Charlie', email: 'charlie@test.com', password: '123', password_confirmation: '123')
+  expect(user.password).to eq(user.password_confirmation)
+end
+
+it 'must have a unique email' do
+  user = User.new(name: 'Charlie', email: 'charlie@test.com', password: '123', password_confirmation: '123')
+  expect(user.save).to be true
+  expect(User.where(email: user.email).count).to eq(1)
+end
+
+it 'must have a name and email' do
+  user = User.new(name: 'Charlie', email: 'charlie@test.com', password: '123', password_confirmation: '123')
+  expect(user.save).to be true
+end
+
+
+
 end
